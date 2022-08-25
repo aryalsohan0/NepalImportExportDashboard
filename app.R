e22339d55991eba3f3d36959923d856e5716a5d7
Sohan Aryal <aryalsohan0@gmail.com>
2022-08-25 18:56:02 +0545

    move last two line out from wellPanel

--- app.R
+++ app.R
@@ -14,6 +14,7 @@ ui <- fluidPage(
 	    
 	    # Overview Panel for info about dashboard
 	    tabPanel('Overview',
+	             
 	             fluidRow(
 	                 column(
 	                     12,
@@ -21,24 +22,28 @@ ui <- fluidPage(
 	                     wellPanel(
 	                         
 	                         h4(
-	                             "This dashboard app visualizes the trade flow of Nepal from different countries from fiscal year 2073/074 to 2077/078 "
+	                             "This dashboard app visualizes the trade flow of Nepal from/to different countries from fiscal year 2073/074 to 2077/078."
 	                         ),
+	                         
 	                         h5(
 	                             "1. Per-commodity tab shows the flow of trade based on the selected commodities.
                                 Specifically, it shows the trend of imports/exports and the top countries for the selected commodities."
 	                         ),
+	                         
 	                         h5(
 	                             "2. Per-country shows the trend of imports/exports with the selected country.
                                 Specifically, it shows the trade balance with the country and also shows commodity-level information."
 	                         )
 	                     ),
+	                     
 	                     br(),
-	                     wellPanel(
+	                     
+	                     
+	                     helpText("Data for this projecty are from", a("Nepal Trade Information Portal", href = "https://nepaltradeportal.gov.np/report", target = "_blank")),
+	                     br(),
+	                     
 	                         
-	                         helpText("Created by", a("Sohan Aryal", href = "https://github.com/aryalsohan0/", target = "_blank")),
-	                         
-	                         helpText("Data for this projecty are from", a("Nepal Trade Information Portal", href = "https://nepaltradeportal.gov.np/report", target = "_blank"))
-	                     )
+	                   helpText("Created by", a("Sohan Aryal", href = "https://github.com/aryalsohan0/", target = "_blank"))
 	                 )
 	             )),
 	    
